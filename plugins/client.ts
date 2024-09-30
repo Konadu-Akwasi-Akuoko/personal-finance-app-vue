42c32442f577e49b8ce5c7f66f018cc4bfecdd34
Akwasi Konadu Akuoko <konaduakwasiakuoko@gmail.com>
2024-09-30 23:05:34 +0000

    feat: added the error link

--- plugins/client.ts
+++ plugins/client.ts
@@ -1,9 +1,13 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "~/server/api/trpc/routers";
+import { errorLink } from "~/utils/trpc-links/errorLink";
 
 export default defineNuxtPlugin(() => {
   const client = createTRPCProxyClient<AppRouter>({
-    links: [httpBatchLink({ url: "http://localhost:3000/api/trpc" })],
+    links: [
+      errorLink,
+      httpBatchLink({ url: "http://localhost:3000/api/trpc" }),
+    ],
   });
 
   return { provide: { client } };
