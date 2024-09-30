9dd9dda65d1cd7583b89c944f883d14d1ea9c2d0
Akwasi Konadu Akuoko <konaduakwasiakuoko@gmail.com>
2024-09-30 16:07:12 +0000

    feat: created the image used on the sign up and login pages

--- components/pages/login/Image.vue
+++ components/pages/login/Image.vue
@@ -4,16 +4,16 @@ import Logo from "~/components/icons/Logo.vue";
 
 <template>
   <div
-    class="h-full w-full max-w-[560px] rounded-t-none bg-login-image bg-cover bg-top bg-no-repeat lg:rounded-xl"
+    class="h-full w-full rounded-t-none bg-login-image bg-cover bg-top bg-no-repeat lg:max-w-[560px] lg:rounded-xl"
   >
     <div class="flex h-full flex-col justify-between p-10">
       <Logo />
 
       <div class="flex flex-col gap-6 text-white">
-        <h2 class="text-4xl font-medium">
+        <h2 class="text-4xl font-medium text-green-500">
           Keep track of your money and save for your future
         </h2>
-        <p class="text-sm font-light">
+        <p class="text-sm font-bold text-green-500 lg:font-light">
           Personal finance app puts you in control of your spending. Track
           transactions, set budgets, and add to savings pots easily.
         </p>
