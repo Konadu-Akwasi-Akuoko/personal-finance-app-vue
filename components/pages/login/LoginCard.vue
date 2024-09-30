cc4d7cd0a3c0bc5eb283f324f4a02202d3dcbc40
Akwasi Konadu Akuoko <konaduakwasiakuoko@gmail.com>
2024-09-30 16:06:57 +0000

    feat: created login card

--- components/pages/login/LoginCard.vue
+++ components/pages/login/LoginCard.vue
@@ -4,10 +4,12 @@ import Input from "~/components/ui/input/Input.vue";
 import Label from "~/components/ui/label/Label.vue";
 import { useForm } from "vee-validate";
 import * as yup from "yup";
-import {useAutoAnimate} from "@formkit/auto-animate/vue";
+import { useAutoAnimate } from "@formkit/auto-animate/vue";
 
-const [parent] = useAutoAnimate()
-const [parent2] = useAutoAnimate()
+const { $client } = useNuxtApp();
+
+const [parent] = useAutoAnimate();
+const [parent2] = useAutoAnimate();
 
 const schema = yup.object({
   email: yup.string().required().email(),
@@ -21,8 +23,14 @@ const { errors, defineField, validateField, handleSubmit } = useForm({
 const [email, emailAttrs] = defineField("email");
 const [password, passwordAttrs] = defineField("password");
 
-const login = handleSubmit((values) => {
+const login = handleSubmit(async (values) => {
   console.log(values);
+  const data = await $client.login.mutate({
+    email: values.email,
+    password: values.password,
+  });
+
+  console.log("data from server: ", data);
 });
 </script>
 
@@ -33,7 +41,7 @@ const login = handleSubmit((values) => {
     >
       <h1 class="text-4xl font-bold">Login</h1>
       <form class="flex w-full flex-col gap-8" @submit.prevent="login()">
-        <div ref="parent2"  class="flex flex-col gap-2" >
+        <div ref="parent2" class="flex flex-col gap-2">
           <Label for="email">Email</Label>
           <Input
             id="email"
@@ -43,9 +51,11 @@ const login = handleSubmit((values) => {
             placeholder="Email"
             @change="validateField('email')"
           />
-          <span v-if="errors.email " class="text-sm font-bold text-red-500">{{ errors.email }}</span>
+          <span v-if="errors.email" class="text-sm font-bold text-red-500">{{
+            errors.email
+          }}</span>
         </div>
-        <div ref="parent" v-auto-animate class="flex flex-col gap-2" >
+        <div ref="parent" class="flex flex-col gap-2">
           <Label for="password">Password</Label>
           <Input
             id="password"
@@ -61,6 +71,14 @@ const login = handleSubmit((values) => {
         </div>
         <Button type="submit">Login</Button>
       </form>
+      <div class="flex flex-row justify-center text-sm font-light">
+        <p>
+          Need to create an account
+          <span class="font-bold text-primary underline underline-offset-1">
+            <NuxtLink to="/sign-up"> Sign up </NuxtLink>
+          </span>
+        </p>
+      </div>
     </div>
   </div>
 </template>
