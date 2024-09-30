9346f007659b54f5766b68bfd655a5a1702126bd
Akwasi Konadu Akuoko <konaduakwasiakuoko@gmail.com>
2024-09-30 16:06:35 +0000

    feat: created sign up page

---
+++ pages/sign-up/index.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="flex h-full w-full flex-col lg:flex-row lg:p-5">
+    <PagesLoginImage />
+    <PagesSignUpCard />
+  </div>
+</template>
